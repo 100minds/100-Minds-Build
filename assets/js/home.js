aea329a4e003f26b03daf6346a8e42ac302c41d8
Tony Ennis <ennis.tony@gmail.com>
2014-07-17 23:20:53 +0100

    New impact page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,34 +1,49 @@
 $(document).ready(function() {	
 
-	$('.fs1').delay(1000).fadeIn( 1000, function() {
-		$('.fs2').typewrite({"delay":30 , "pause": 0, "callback" : function() {
-			$('.fs3').typewrite({"delay":30, "pause":500, "callback" : function() {
-				$(".fs4").typewrite({"delay":30, "pause": 700, "callback" : function() {
-					$(".fs1").slideUp()
-					$(".fs5").show().animate({fontSize : '250px' }, 400, function() {
-						setTimeout(function() {
-							$(".fs6").typewrite({"delay":30, "pause": 500, "callback" : function() {
-								$(".fs2, .fs3").slideUp();
-								$(".fs7").typewrite({"delay":30, "pause": 500, "callback" : function() {
-									$(".fs4, .fs5").slideUp()
-									$(".fs8").show().animate({fontSize: '225px'},400, function() {
-										setTimeout(function() {
-											$(".fs6").slideUp()
-											$(".fs9").typewrite({"delay":30, "callback": function() {
-												$(".fs10").typewrite({"delay":30, "callback" : function() {
-													$("#arrow-down").fadeIn()
-												}})
-											}})
-										}, 500)
-									})
-								}})
-							}})
-						}, 100)
-					})
+
+
+	$('.fs0').delay(1000).fadeIn( 1000, function() {	
+		$('.fs1').delay(1000).fadeIn( 1000, function() {
+			$('.fs2').typewrite({"delay":30 , "pause": 0, "callback" : function() {
+				$('.fs3').typewrite({"delay":30, "pause":500, "callback" : function() {
+					$(".fs4").typewrite({"delay":30, "pause": 700, "callback" : function() {
+						
+					}})
 				}})
 			}})
-		}})
+		})
 	})
+
+
+// $('.fs1').delay(1000).fadeIn( 1000, function() {
+// 		$('.fs2').typewrite({"delay":30 , "pause": 0, "callback" : function() {
+// 			$('.fs3').typewrite({"delay":30, "pause":500, "callback" : function() {
+// 				$(".fs4").typewrite({"delay":30, "pause": 700, "callback" : function() {
+// 					$(".fs1").slideUp()
+// 					$(".fs5").show().animate({fontSize : '250px' }, 400, function() {
+// 						setTimeout(function() {
+// 							$(".fs6").typewrite({"delay":30, "pause": 500, "callback" : function() {
+// 								$(".fs2, .fs3").slideUp();
+// 								$(".fs7").typewrite({"delay":30, "pause": 500, "callback" : function() {
+// 									$(".fs4, .fs5").slideUp()
+// 									$(".fs8").show().animate({fontSize: '225px'},400, function() {
+// 										setTimeout(function() {
+// 											$(".fs6").slideUp()
+// 											$(".fs9").typewrite({"delay":30, "callback": function() {
+// 												$(".fs10").typewrite({"delay":30, "callback" : function() {
+// 													$("#arrow-down").fadeIn()
+// 												}})
+// 											}})
+// 										}, 500)
+// 									})
+// 								}})
+// 							}})
+// 						}, 100)
+// 					})
+// 				}})
+// 			}})
+// 		}})
+// 	})
 
 	$("#arrow-down").click(function() {
 
